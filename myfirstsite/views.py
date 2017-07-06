000aad3210aefef9dfd791bb5c6263f9993ed994
uvirus <umesh.pandey994@gmail.com>
2017-07-06 15:03:54 +0530

    MyFirstSite
    MyFirstSiteProject

---
+++ myfirstsite/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from datetime import datetime
+from .models import Article
+#from django.templates import RequestContext
+def index(requst):
+    context={
+
+        'current_date':datetime.now(),
+        'title':'Home'
+    }
+    return render(requst,'index.html',context)
+
+def about(request):
+    context={
+        'current_date':datetime.now(),
+        'title':'About'
+
+    }
+    return render(request,'about.html',context)
+
+def news(request):
+    populate_db()
+    articles=get_articles()
+
+    context={
+        'articles':articles,
+        'current_date':datetime.now(),
+        'title':'News'
+
+    }
+    return render(request,'news.html',context)
+
+
+def get_articles():
+    result=Article.objects.all()
+    return result
+
+def populate_db():
+    if Article.objects.count==0:
+        Article(title="first item",content="this is the first item").save()
+        Article(title="second item", content="this is the second item").save()
+        Article(title="third item", content="this is the third item").save()
+
